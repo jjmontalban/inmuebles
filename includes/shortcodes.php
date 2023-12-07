59d3f17ecb887e6c980b58831a036cd3af0dd250
JJMontalban <jjmontalban@gmail.com>
2023-12-07 10:15:15 +0100

    update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -4,7 +4,6 @@
 function formulario_contacto_shortcode() {
     ob_start(); ?>
 
-    <div class="form-shortcode-container">
         <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="formulario-contacto">
             <input type="hidden" name="action" value="procesar_formulario_contacto">
             <?php if (is_singular('inmueble')) : ?>
@@ -17,18 +16,18 @@ function formulario_contacto_shortcode() {
                 <h5>Dinos quién eres y te contactamos:</h5>
                 <input type="hidden" value="page">
             <?php endif; ?>
-                Nombre: <input type="text" name="nombre" required><br>
-                Email (opcional): <input type="email" name="email"><br>
-                Teléfono (obligatorio): <input type="tel" name="telefono" required><br>
-                Mensaje (opcional): <textarea name="mensaje"></textarea><br>
+                <input type="text" name="nombre" placeholder="Nombre" required ><br>
+                <input type="email" name="email" placeholder="Email (opcional)"><br>
+                <input type="tel" name="telefono" placeholder="telefono" required><br>
+                <textarea name="mensaje" placeholder="Mensaje (opcional)" style="height: 100px;"></textarea><br>
                 <input type="submit" value="Enviar">
         </form>
         <div id="respuesta-formulario-contacto"></div>
-    </div>
-
     <?php
     return ob_get_clean();
-}
+}   
+
+
 
 // Registrar el shortcode
 add_shortcode('formulario_contacto', 'formulario_contacto_shortcode');
