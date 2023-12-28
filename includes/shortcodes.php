4d275a98fefd0ac0a33637a60ce73a7b7b636687
JJMontalban <jjmontalban@gmail.com>
2023-12-28 10:43:25 +0100

    Refactoring shortcode ultimos_inmuebles

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,13 +1,15 @@
 <?php 
 
-
-
-function formulario_contacto_shortcode() {
+/**
+ * Shortcode contacto [formulario_contacto]
+ */
+function formulario_contacto_shortcode() 
+{
     ob_start();
     ?>
 
     <style>
-        /* Estilos para los botones */
+
         .contact-link,
         .submit-button {
             display: block;
@@ -19,7 +21,18 @@ function formulario_contacto_shortcode() {
             border-radius: 5px;
             margin-top: 10px;
         }
-        
+
+        .submit-button {
+            background-color: #eb5d0b;
+            margin-top: 20px;
+            border: none;
+            cursor: pointer;
+        }
+
+        .contact-link:hover,
+        .submit-button:hover {
+            opacity: 0.8;
+        }
 
         .whatsapp-link {
             background-color: #25d366;
@@ -30,27 +43,12 @@ function formulario_contacto_shortcode() {
             background-color: #ccc;
         }
 
-        .submit-button {
-            background-color: #eb5d0b;
-            margin-top: 20px; /* Ajusta el margen superior del botón Enviar */
-        }
 
-        /* Estilos para todos los botones al pasar el mouse */
-        .contact-link:hover,
-        .submit-button:hover {
-            opacity: 0.8;
-        }
-
-        /* Estilos para los iconos */
         .contact-link i {
             margin-right: 5px;
             vertical-align: middle;
         }
 
-        .submit-button {
-            border: none;
-            cursor: pointer;
-        }
     </style>
    
 
@@ -76,11 +74,11 @@ function formulario_contacto_shortcode() {
 
         <!-- Agregar un campo oculto en el formulario -->
         <input type="text" name="extra_field" style="display: none;">
-
     </form>
+
     <div id="respuesta-formulario-contacto"></div>
     
-    <!-- Para mostrar los dos últimos enlaces solo en las páginas de tipo de publicación "inmueble" o en la página de archivo de tipo "inmueble" -->
+    <!-- Para mostrar los dos últimos botones solo en las páginas de inmueble, archivo de inmuebles y -->
     <?php if (is_singular('inmueble') || is_post_type_archive('inmueble')) : ?>
 
         <!-- Botón de WhatsApp con icono -->
@@ -98,6 +96,7 @@ function formulario_contacto_shortcode() {
     <?php endif; ?>
 
     <script>
+
         jQuery(document).ready(function($) {
             $('#formulario-contacto').submit(function(e) {
                 e.preventDefault();
@@ -108,7 +107,7 @@ function formulario_contacto_shortcode() {
                     data: $(this).serialize(),
                     success: function(response) {
                         $('#respuesta-formulario-contacto').html('<p style="color: green; background-color: white;">¡Consulta enviada con éxito! Nos pondremos en contacto contigo lo antes posible.</p>');
-                        $('#formulario-contacto')[0].reset(); // Limpia los campos del formulario
+                        $('#formulario-contacto')[0].reset();
                     },
                     error: function(error) {
                         $('#respuesta-formulario-contacto').html('<p style="color: red;">Ha ocurrido un error al enviar la consulta.</p>');
@@ -122,3 +121,142 @@ function formulario_contacto_shortcode() {
     return ob_get_clean();
 }   
 add_shortcode('formulario_contacto', 'formulario_contacto_shortcode');
+
+
+
+/**
+ * Shortcode ultimos-inmuebles [ultimos_inmuebles]
+ */
+function ultimos_inmuebles_shortcode() 
+{
+    ob_start();
+    ?>
+
+    <style>
+        .latest-inmuebles {
+            padding-top: 2%;
+            text-align: center;
+            
+        }
+
+        .latest-inmuebles .inmueble-row {
+            flex-wrap: wrap;
+            display: flex;
+            justify-content: space-between;
+        }
+    
+        .inmueble-item {
+            width: 32%;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            position: relative;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+            transition: box-shadow 0.3s ease;
+            overflow: hidden;
+            height: 400px;
+        }
+
+        .inmueble-item img {
+            width: 100%;
+            height: 40%;
+            object-fit: cover; 
+            margin-bottom: inherit;
+        }
+    
+        .inmueble-precio {
+            position: absolute;
+            border-radius: 5px;
+            top: 10px;
+            left: 10px;
+            background: rgba(255, 255, 255, 0.7);
+            font-weight: bold;
+            padding: 5px 10px; 
+        }
+    
+        .inmueble-info {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            margin-bottom: 20px;
+            position: relative;
+        }
+
+        .btn-inmueble {
+            display: block;
+            background-color: #333;
+            border-radius: 5px;
+            color: #fff;
+            font-size: 0.8em;
+            width: 50%;
+            margin-left: 25%;
+            text-align: center;
+            padding: 5px 10px;
+            text-decoration: none;
+            transition: background-color 0.3s;
+            margin-bottom: 4%;
+        }
+    
+        .btn-inmueble:hover {
+            color: #fff;
+            background-color: #eb5d0b;
+        }
+    
+        @media (max-width: 768px) {
+            .inmueble-item {
+                width: 100%;
+            }
+            .info-item {
+                width: 100%;
+            }
+        }
+    
+    </style>
+
+    <section class="latest-inmuebles">
+        <h3>Últimos inmuebles añadidos</h3>
+        <?php
+        $args = array(
+            'post_type' => 'inmueble',
+            'posts_per_page' => 9,
+            'orderby' => 'date',
+            'order' => 'DESC'
+        );
+        $query = new WP_Query($args);
+        global $tipos_inmueble_map;
+    
+        if ($query->have_posts()): ?>
+            <div class="inmueble-row">
+                <?php while ($query->have_posts()): $query->the_post(); ?>
+                    <?php $campos = obtener_campos_inmueble(get_the_ID());  ?>
+                    <div class="inmueble-item">
+                        <!-- Mostrar la imagen por defecto -->
+                        <?php if (has_post_thumbnail()): ?>
+                            <?php the_post_thumbnail(); ?>
+                        <?php endif; ?>
+                        <div class="inmueble-precio">
+                            <p class="inmuebles-price">
+                                <?php if ($campos['precio_venta']) : ?>
+                                    <?php echo $campos['precio_venta']; ?> €
+                                <?php else: ?>
+                                    <?php echo $campos['precio_alquiler']; ?> €/mes
+                                <?php endif; ?>
+                            </p>
+                        </div>
+                        <h5><?php echo $tipos_inmueble_map[$campos['tipo_inmueble']] . ' en ' . $campos['nombre_calle'] . ', ' . $campos['localidad']; ?></h5>
+                        <div class="inmueble-info">
+                            <span><?php echo $campos['m_construidos'] . ' m2'; ?></span>
+                            <span><?php echo $campos['num_dormitorios'] . ' Dorm'; ?></span>
+                            <span><?php echo $campos['num_dormitorios'] . ' Baño'; ?></span>
+                        </div>
+                        <a href="<?php the_permalink(); ?>" class="btn-inmueble">Ver inmueble</a>
+
+
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; wp_reset_postdata(); ?>
+    </section>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('ultimos_inmuebles', 'ultimos_inmuebles_shortcode');
