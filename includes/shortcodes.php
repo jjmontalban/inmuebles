1de2a510ef4cb7a6b7df400cb5ff70990bced071
JJMontalban <jjmontalban@gmail.com>
2024-03-27 17:38:27 +0100

    shortcodes

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -101,34 +101,67 @@ function formulario_comprar_shortcode()
 {
     ob_start();
     ?>
-    <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="formulario-comprar">
-        <input type="hidden" name="action" value="procesar_formulario_contacto">
-        <input type="hidden" name="tipo_formulario" value="comprar">
-        <h5 class="form-ppal">Rellena el siguiente formulario y obtén un plano informativo con las últimas transacciones de inmuebles realizadas en Chipiona</h5>
-        <input type="text" name="nombre" placeholder="Nombre y apellidos*" required>
-        <input type="email" name="email" placeholder="Email*" required>
-        <input type="tel" name="telefono" placeholder="Teléfono*" required>
-        <select name="zona">
-            <option value="" selected>Zona deseada</option>
-            <?php 
-            global $zonas_inmueble_map;
-            foreach ($zonas_inmueble_map as $key => $value) {
-                echo '<option value="' . esc_attr($key) . '">' . esc_html($value) . '</option>';
-            }
-            ?>
-        </select>
-        <input type="text" name="presupuesto" placeholder="Presupuesto">
-        <textarea name="mensaje" placeholder="Mensaje (opcional)" style="height: 100px;"></textarea>
-        <input type="checkbox" name="aceptar_condiciones" required>
-        <label for="aceptar_condiciones" style="font-size: 0.8em;">Usando este formulario estás aceptando nuestra <a target="blank" href="<?php echo esc_url(get_permalink(get_page_by_path('aviso-legal'))); ?>">política de privacidad</a></label>
-        <button type="submit" class="submit-button">
-            Enviar
-            <span class="spinner" style="display: none;"><i class="fa fa-spinner fa-spin"></i></span>
-        </button>
-        <!-- Agregar un campo oculto en el formulario -->
-        <input type="text" name="extra_field" style="display: none;">
-        <!-- Agregar un campo oculto para almacenar el token de reCAPTCHA v3 -->
-        <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response-comprar" value="">
+    <form class="form-a contactForm" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="formulario-comprar">
+        <div class="row">
+            <input type="hidden" name="action" value="procesar_formulario_contacto">
+            <input type="hidden" name="tipo_formulario" value="comprar">
+            <h5 class="icon-title">Rellena el siguiente formulario y obtén un plano informativo con las últimas transacciones de inmuebles realizadas en Chipiona</h5>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="text" name="nombre" placeholder="Nombre y apellidos*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Email*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="tel" name="telefono" placeholder="Teléfono*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <select name="zona" class="form-control form-control-lg form-control-a" >
+                        <option value="" selected>Zona deseada</option>
+                        <?php 
+                        global $zonas_inmueble_map;
+                        foreach ($zonas_inmueble_map as $key => $value) {
+                            echo '<option value="' . esc_attr($key) . '">' . esc_html($value) . '</option>';
+                        }
+                        ?>
+                </select>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="text" name="presupuesto" class="form-control form-control-lg form-control-a" placeholder="Presupuesto" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <textarea name="mensaje" class="form-control" name="message" cols="45" rows="8" placeholder="Mensaje (opcional)"></textarea>
+                </div>
+            </div>            
+            <div class="col-md-12">
+                <div class="form-group">
+                    <input type="checkbox" name="aceptar_condiciones" required>
+                    <label for="aceptar_condiciones" style="font-size: 0.8em;">Usando este formulario estás aceptando nuestra <a target="blank" href="<?php echo esc_url(get_permalink(get_page_by_path('aviso-legal'))); ?>">política de privacidad</a></label>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-a">
+                        Enviar
+                        <span class="spinner" style="display: none;"><i class="fa fa-spinner fa-spin"></i></span>
+                    </button>
+                </div>
+            </div>
+            <input type="text" name="extra_field" style="display: none;">
+            <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response-comprar" value="">
+        </div>
+
     </form>
     <div id="respuesta-formulario-comprar"></div>
     <?php
@@ -144,25 +177,55 @@ function formulario_vender_shortcode()
 {
     ob_start();
     ?>
-    <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="formulario-vender">
-        <input type="hidden" name="action" value="procesar_formulario_contacto">
-        <input type="hidden" name="tipo_formulario" value="vender">
-        <h5 class="form-ppal">Rellena el siguiente formulario y obtén un plano informativo con las últimas transacciones de inmuebles realizadas en Chipiona</h5>
-        <input type="text" name="nombre" placeholder="Nombre y apellidos*" required>
-        <input type="email" name="email" placeholder="Email*" required>
-        <input type="tel" name="telefono" placeholder="Teléfono*" required>
-        <input type="text" name="direccion" placeholder="dirección de la vivienda">
-        <textarea name="mensaje" placeholder="Mensaje (opcional)" style="height: 100px;"></textarea>
-        <input type="checkbox" name="aceptar_condiciones" required>
-        <label for="aceptar_condiciones" style="font-size: 0.8em;">Usando este formulario estás aceptando nuestra <a target="blank" href="<?php echo esc_url(get_permalink(get_page_by_path('aviso-legal'))); ?>">política de privacidad</a></label>
-        <button type="submit" class="submit-button">
-            Enviar
-            <span class="spinner" style="display: none;"><i class="fa fa-spinner fa-spin"></i></span>
-        </button>
-        <!-- Agregar un campo oculto en el formulario -->
-        <input type="text" name="extra_field" style="display: none;">
-        <!-- Agregar un campo oculto para almacenar el token de reCAPTCHA v3 -->
-        <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response-vender" value="">
+    <form class="form-a contactForm" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="formulario-vender">
+        <div class="row">
+            <input type="hidden" name="action" value="procesar_formulario_contacto">
+            <input type="hidden" name="tipo_formulario" value="vender">
+            <h5 class="icon-title">Rellena el siguiente formulario y obtén un plano informativo con las últimas transacciones de inmuebles realizadas en Chipiona</h5>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="text" name="nombre" placeholder="Nombre y apellidos*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Email*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="tel" name="telefono" placeholder="Teléfono*" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <input type="text" name="direccion" placeholder="dirección de la vivienda" class="form-control form-control-lg form-control-a" required>
+                </div>
+            </div>
+            <div class="col-md-12 mb-3">
+                <div class="form-group">
+                    <textarea name="mensaje" class="form-control" name="message" cols="45" rows="8" placeholder="Mensaje (opcional)"></textarea>
+                </div>
+            </div>            
+            <div class="col-md-12">
+                <div class="form-group">
+                    <input type="checkbox" name="aceptar_condiciones" required>
+                    <label for="aceptar_condiciones" style="font-size: 0.8em;">Usando este formulario estás aceptando nuestra <a target="blank" href="<?php echo esc_url(get_permalink(get_page_by_path('aviso-legal'))); ?>">política de privacidad</a></label>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-a">
+                        Enviar
+                        <span class="spinner" style="display: none;"><i class="fa fa-spinner fa-spin"></i></span>
+                    </button>
+                </div>
+            </div>
+            <!-- Agregar un campo oculto en el formulario -->
+            <input type="text" name="extra_field" style="display: none;">
+            <!-- Agregar un campo oculto para almacenar el token de reCAPTCHA v3 -->
+            <input type="hidden" name="g-recaptcha-response" id="g-recaptcha-response-vender" value="">
+        </div>
     </form>
     <div id="respuesta-formulario-vender"></div>
     <?php
@@ -179,8 +242,6 @@ function ultimos_inmuebles_shortcode()
 {
     ob_start();
     ?>
-    <section class="latest-inmuebles">
-        <h3>Últimos inmuebles añadidos</h3>
         <?php
         $args = array(
             'post_type' => 'inmueble',
@@ -190,40 +251,88 @@ function ultimos_inmuebles_shortcode()
         );
         $query = new WP_Query($args);
         global $tipos_inmueble_map;
-    
         if ($query->have_posts()): ?>
-            <div class="inmueble-row">
-                <?php while ($query->have_posts()): $query->the_post(); ?>
-                    <?php $campos = obtener_campos_inmueble(get_the_ID());  ?>
-                    <div class="inmueble-item">
-                        <!-- Mostrar la imagen por defecto -->
-                        <?php if (has_post_thumbnail()): ?>
-                            <?php the_post_thumbnail(); ?>
-                        <?php endif; ?>
-                        <div class="inmueble-precio">
-                            <p class="inmuebles-price">
-                                <?php if ($campos['precio_venta']) : ?>
-                                    <?php echo $campos['precio_venta']; ?> €
-                                <?php else: ?>
-                                    <?php echo $campos['precio_alquiler']; ?> €/mes
-                                <?php endif; ?>
-                            </p>
-                        </div>
-                        <h5><?php echo $tipos_inmueble_map[$campos['tipo_inmueble']] . ' en ' . $campos['nombre_calle']; ?></h5>
-                        <div class="inmueble-info">
-                            <span><?php echo $campos['m_construidos'] . ' m2'; ?></span>
-                            <span><?php echo $campos['num_dormitorios'] . ' Dorm'; ?></span>
-                            <span><?php echo $campos['num_dormitorios'] . ' Baño'; ?></span>
-                        </div>
-                        <a href="<?php the_permalink(); ?>" class="btn-inmueble">Ver inmueble</a>
+            <section class="property-grid grid">
+                <div class="container">
+                    <div class="row">
 
-
+                        <div class="col-md-1"></div>
+                        
+                        <div class="col-md-10">
+                            <div class="row">
+                                <?php while ($query->have_posts()): $query->the_post(); ?>
+                                <?php 
+                                    $tipo_inmueble = get_post_meta(get_the_ID(), 'tipo_inmueble', true);
+                                    $campos = obtener_campos_inmueble(get_the_ID());
+                                    ?> 
+                                    <div class="col-md-4 col-6">
+                                        <div class="card-box-a card-shadow">
+                                            <div class="img-box-a">
+                                                <?php if (has_post_thumbnail()) : ?>
+                                                    <?php the_post_thumbnail('medium', ['class' => 'img-a img-fluid']); ?>
+                                                <?php endif; ?>
+                                            </div>
+                                            <div class="card-overlay">
+                                                <div class="card-overlay-a-content">
+                                                    <div class="card-header-a">
+                                                        <h2 class="card-title-a">
+                                                            <a href="<?php the_permalink(); ?>">
+                                                                <?php echo esc_html__( $tipos_inmueble_map[$tipo_inmueble], 'chipicasa' ); ?>
+                                                                <br /> <?php echo esc_html__( 'in', 'chipicasa' ); ?> <?php echo esc_html__( $campos['nombre_calle'], 'chipicasa' ); ?>
+                                                            </a>
+                                                        </h2>
+                                                    </div>
+                                                    <div class="card-body-a">
+                                                        <div class="price-box d-flex">
+                                                            <span class="price-a">
+                                                                <?php if ($campos['precio_venta']) : ?>
+                                                                    <?php echo $campos['precio_venta']; ?> €
+                                                                <?php else: ?>
+                                                                    <?php printf( esc_html__( '%s €/mes', 'chipicasa' ), $campos['precio_alquiler'] ); ?>
+                                                                <?php endif; ?>
+                                                            </span>
+                                                        </div>
+                                                        <a href="<?php the_permalink(); ?>" class="link-a">
+                                                            <?php echo esc_html__( 'Click here to view', 'chipicasa' ); ?>
+                                                            <span class="ion-ios-arrow-forward"></span>
+                                                        </a>
+                                                    </div>
+                                                    <div class="card-footer-a">
+                                                        <ul class="card-info d-flex justify-content-around">
+                                                            <li>
+                                                                <?php if ($campos['m_construidos']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Area', 'chipicasa' ); ?></h4>
+                                                                    <span>
+                                                                        <span><?php echo $campos['m_construidos']; ?></span>m<sup>2</sup>
+                                                                    </span>
+                                                                <?php endif; ?> 
+                                                            </li>
+                                                            <li>
+                                                                <?php if ($campos['num_dormitorios']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Beds', 'chipicasa' ); ?></h4>
+                                                                    <span><?php echo $campos['num_dormitorios']; ?></span>
+                                                                <?php endif; ?>
+                                                            </li>
+                                                            <li>
+                                                                <?php if ($campos['num_banos']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Baths', 'chipicasa' ); ?></h4>
+                                                                    <span><?php echo $campos['num_banos']; ?></span>
+                                                                <?php endif; ?>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endwhile; ?>
+                            </div>
+                        </div>
+                        <div class="col-md-1"></div>
                     </div>
-                <?php endwhile; ?>
-            </div>
-        <?php endif; wp_reset_postdata(); ?>
-    </section>
-    <?php
+                </div>
+            </section>
+        <?php endif; wp_reset_postdata();
     return ob_get_clean();
 }
 add_shortcode('ultimos_inmuebles', 'ultimos_inmuebles_shortcode');
